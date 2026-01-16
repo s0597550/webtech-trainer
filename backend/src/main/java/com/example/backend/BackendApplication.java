89a2fd71bbf11cf4e84d53a7b726f7d2cbbb2e08
s0597550 <batuhanzeybekoglu78@gmail.com>
2026-01-16 21:14:20 +0100

    test

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,12 +1,14 @@
-package com.example.backend;  /
+package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.backend.game")
 public class BackendApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 }
-
