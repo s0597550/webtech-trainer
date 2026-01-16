ee301646c6756c461fdeaf57a335aaad53c45b3b
s0597550 <batuhanzeybekoglu78@gmail.com>
2026-01-16 23:20:55 +0100

    Finale commits

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.backend.game")
 public class BackendApplication {
 
     public static void main(String[] args) {
